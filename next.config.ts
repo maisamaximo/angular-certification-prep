65ab674e331594bbddd87dc4530b33dc2a0e753c
maisamaximo <ferreira.maximo.maisa@gmail.com>
2025-12-14 03:38:54 +0000

    Deploy to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
+
+const repo = "angular-certification-prep"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    output: "export",
+    trailingSlash: true,
+    images: {
+        unoptimized: true
+    },
+    basePath: `/${repo}`,
+    assetPrefix: `/${repo}/`
+}
 
-export default nextConfig;
+export default nextConfig
